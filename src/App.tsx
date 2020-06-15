599ac30e9e66a7f3ab32d65651c81d8ee3a24e52
Anhar Uddin <anharhussain94@gail.com>
2020-06-15 23:57:15 +0100

    feat: set current month

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Calendar from './components/calendar';
 
 function App() {
+
+  const [year, setYear] = useState(2020);
+  const [month, setMonth] = useState(6);
+
+  useEffect(() => {
+    const today = new Date();
+    setYear(today.getFullYear());
+    setMonth(today.getMonth() + 1);
+}, []);
+
   return (
     <>
-      <Calendar year={2020} month={6} />
+      <Calendar year={year} month={month} />
     </>
   );
 }
